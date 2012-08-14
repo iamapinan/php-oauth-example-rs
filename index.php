23174537e471a06d39148c4a61e5a081d6b1b04e
F. Kooman <fkooman@tuxed.net>
2012-08-14 14:03:34 +0200

    update README, return JSON instead of plain text

--- index.php
+++ index.php
@@ -4,19 +4,21 @@ require_once "RemoteResourceServer.php";
 
 $tokenEndpoint = 'http://localhost/php-oauth/token.php';
 
-if(array_key_exists("action", $_GET) && $_GET['action'] === 'protected') {
-    // restricted call
+header("Content-Type: application/json");
+
+if(array_key_exists("protected", $_GET) && $_GET['protected'] === "1") {
+    // restricted call, needs authorization
     try {
         $rs = new RemoteResourceServer($tokenEndpoint);
-	$headers = apache_request_headers();
-	$ah = array_key_exists('Authorization', $headers) ? $headers['Authorization'] : NULL;
-        $v = $rs->verify($ah);
-        echo "Welcome to the protected resource " . $v['resource_owner_id'] . "!";
+        $headers = apache_request_headers();
+        $ah = array_key_exists('Authorization', $headers) ? $headers['Authorization'] : NULL;
+        $token = $rs->verify($ah);
+        echo json_encode(array("authorized" => TRUE, "resource_owner_id" => $token['resource_owner_id']));    
     } catch (VerifyException $ve) {
-        echo $ve->getMessage() . ": " . $ve->getDescription();
+        echo json_encode(array("error" => $ve->getMessage(), "error_description" => $ve->getDescription()));
     }
 } else {
     // unrestricted call
-    echo "Hello World!";
-}
+    echo json_encode(array("authorized" => FALSE, "message" => "Hello World!"));
+}  
 ?>
